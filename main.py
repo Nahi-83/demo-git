3897ee93529684601a175a956c6c44d7a88dc239
Nahiara Millahuala <nahi042007@gmail.com>
2025-08-12 10:03:07 -0300

    Se modifico el main

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while True:
     print("MENÚ DEL DRON")
     print("1. Ver datos del dron")
     print("2. Volar")
-    print("3. Cargar batería")
+    print("3. Sector para Cargar batería")
     print("4. Activar cámara")
     print("5. Salir")
 
